797c2937380d2cabf7a1e12d814f1de7503c39a9
Angelin Lalev <lalev@uni-svishtov.bg>
2022-06-09 09:27:44 +0300

    Caesar cipher finished

---
+++ src/main/java/uk/co/lalev/enigmalabs/enigmalab/FreqCounter.java
@@ -0,0 +1,11 @@
+package uk.co.lalev.enigmalabs.enigmalab;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FreqCounter {
+
+    public static Map<Character, Integer> count(String s) {
+        return new HashMap<Character, Integer>();
+    }
+}
